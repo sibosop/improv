4c094cbebcae18de2d481382f37e94a7b060619c
sibosop <brian@eastshore.com>
2019-09-08 16:32:28 -0700

    improv now working in autoplay mode

--- improv.py
+++ improv.py
@@ -8,18 +8,53 @@ import argparse
 import datetime
 import signal
 import traceback
-
+import pygame
+    
+pname = sys.argv[0]
+os.environ['DISPLAY']=":0.0"
 home = os.environ['HOME']
-proj = "%s/%s"%(home,"GitProjects")
-mod = "%s/%s"%(proj,"improv")
-specDir = "%s/%s"%(mod,"specs")
-sys.path.append(mod+"/sibcommon")
-defaultSpecFile = "%s/%s"%(specDir,"improv.json")
-import config
+os.chdir(os.path.dirname(sys.argv[0]))
+sys.path.append("sibcommon")
+defaultSpecFile = "%s/%s"%("specs","improv.json")
+from specs import Specs
 from utils import print_dbg
 from utils import setDebug
-
+from soundFile import SoundFile
+from soundPlayer import PlayerThread
+from soundTrack import SoundTrackManager
 from controller import Controller
+
+defaultSoundDir = "%s/%s"%(home,"/sibosopLocal/music/Music20161008/Clips/schlubFull/")
+
+takesDir = ""
+
+def makeTakesDir():
+  global takesDir
+  print "creating takes dir"
+  rootDir = "GardenTakes/*.*"
+  i = 0
+  done = False
+  files = glob.glob(rootDir)
+  while True:
+    found = False
+    for f in files:
+      #print f
+      try:
+        n = f.rindex(".")
+      except:
+        continue
+      test = int(f[n+1:])
+      #print "test:",test
+      if test == i:
+        found = True
+        i += 1
+        break
+    if not found:
+      takesDir = "GardenTakes/Take.%d"%i
+      print "making",takesDir
+      os.mkdir(takesDir)
+      break
+
 
 class ServiceExit(Exception):
     """
@@ -35,33 +70,64 @@ def service_shutdown(signum, frame):
 
 signal.signal(signal.SIGTERM, service_shutdown)
 signal.signal(signal.SIGINT, service_shutdown)
-controller = None
 
 def slider(e):
   print_dbg("%s callback num %d value %d"%(e.event,e.num,e.value))
-
-
+  
 if __name__ == '__main__':
   try:
     random.seed()
-    pname = sys.argv[0]
-    os.environ['DISPLAY']=":0.0"
-    os.chdir(os.path.dirname(sys.argv[0]))
-    print_dbg(pname+" at "+datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))  
+    print_dbg(pname+" at "+datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')) 
+    pygame.mixer.pre_init(frequency=44100, size=-16, channels=2, buffer=4096)
+    pygame.init()
+     
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--spec', nargs=1, help='specify spec file', default=[defaultSpecFile])
     parser.add_argument('-v','--verbose', action = 'store_true',help='set verbose mode')
+    parser.add_argument('-o','--output', action = 'store_true',help='save session to GardenTakes directory')
+    parser.add_argument('-d','--soundDir',nargs=1,help='specify sound directory',default=[defaultSoundDir])
     args = parser.parse_args()
     setDebug(args.verbose)
-    specFile = args.spec[0]
-    print_dbg("using spec: %s"%specFile)
-    specs = config.load(specFile)
-    controller = Controller(specs["inPort"],specs["outPort"],specs['nano'])
-    controller.register("slider",slider)
-    controller.run()
+    if args.output:
+      makeTakesDir()   
+    print "takesDir:",takesDir 
+  
+# Singletons
+    print_dbg("using spec: %s"%args.spec[0])
+    Specs(args.spec[0])
+    SoundFile()
+    SoundTrackManager(args.soundDir[0])
+    Controller('nano').register("slider",slider)
+    
+    pt = PlayerThread()
+    pt.setDaemon(True)
+    pt.start()
+    while True:
+      time.sleep(1)
+      if pt.done:
+        break
+  
+    print "waiting for channels to be done"
+    while True:
+      n = pygame.mixer.get_busy()
+      print "number busy channels",n
+      if n == 0:
+        break;
+      time.sleep(1)
+    
+    if takesDir != "":
+      for t in SoundTrackManager().eventThreads:
+        desc = json.dumps(t.playList)
+        fname = takesDir+"/"+t.name+".json"
+        print "saving:",fname
+        f = open(fname,"w")
+        f.write(desc)
+        f.close()
+    print "garden done"
+
+    
   except ServiceExit:
     print("Exiting on interrupt")
   except Exception, e:
     traceback.print_exc()
-  if controller:
-    controller.close()
+  
